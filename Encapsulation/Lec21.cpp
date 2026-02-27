b4c07ea505eed574a70a4feb2b372a46640da722
Abhishek9529 <abhishekchavhan61047@gmail.com>
2026-02-27 23:08:13 +0530

    Add more examples

--- Encapsulation/Lec21.cpp
+++ Encapsulation/Lec21.cpp
@@ -1,32 +1,107 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-class Adder{
-	public:
-		Adder(int i =0){
-			total = i;
-		}
-		
-		//interface for outside world
-		void addNum(int number){
-			total += number;
-		}
-		
-		int getTotal(){
-			return total;
-		}
-		
+//P1
+//class Adder{
+//	public:
+//		Adder(int i =0){
+//			total = i;
+//		}
+//		
+//		//interface for outside world
+//		void addNum(int number){
+//			total += number;
+//		}
+//		
+//		int getTotal(){
+//			return total;
+//		}
+//		
+//	private:
+//		//hidden from the outside world 
+//		int total;
+//};
+//
+//int main(){
+//	Adder a;
+//	a.addNum(10);
+//	a.addNum(30);
+//	a.addNum(70);
+//	
+//	cout<<"Total " <<a.getTotal()<< endl;
+//	return 0;
+//}
+
+//Case Study: Encapsulation:-  Student Grade Management System
+
+class Student{
 	private:
-		//hidden from the outside world 
-		int total;
+		string name;
+		int rollNumber;
+		char grade;
+	
+	public:
+		Student(string name, int rollNumber, char grade ){
+			this->name = name;
+			this->rollNumber = rollNumber;
+			setGrade(grade);
+		}
+		
+		// Setter functions
+		void setName(string name){
+			this->name = name;
+		}
+		void setRollNumber(int rollNumber){
+			this->rollNumber = rollNumber;
+		}
+		void setGrade(char grade){
+			if(grade >= 'A' && grade <= 'F'){
+				this->grade = grade;
+			} else{
+				cout<<" Invalid grade! grade must be between A and F"<< endl;
+			}
+			
+		}
+		
+		//Getter functions
+		string getName(){
+			return name;
+		}
+		int getRollNumber(){
+			return rollNumber;
+		}
+		char getGrade(){
+			return grade;
+		}
+		
+		// Method to display student details
+		void displayStudentDetails(){
+			cout<< "Student Name: " << name <<endl;
+			cout<< "Roll Number: " << rollNumber <<endl;
+			cout<< "Grade: " << grade <<endl;
+		}
 };
 
+// Main function to demonstrate encapsulation
 int main(){
-	Adder a;
-	a.addNum(10);
-	a.addNum(30);
-	a.addNum(70);
+	// Creating a student object with valid grade 
+	Student s1("Pradeep", 101, 'A');
+	s1.displayStudentDetails();
 	
-	cout<<"Total " <<a.getTotal()<< endl;
-	return 0;
+	s1.setName("Himanshu");
+	s1.setGrade('B');
+	
+	cout<<"Updated student details: "<<endl;
+	s1.displayStudentDetails();
+	cout<<endl;
+	
+	//Attempt to set an invalid grade
+	s1.setGrade('Z');
+	return 0; 
 }
+
+
+
+
+
