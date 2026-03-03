34f477aa1f734f83416de904d670d0e650c0a0dc
Abhishek9529 <abhishekchavhan61047@gmail.com>
2026-03-03 13:18:54 +0530

    Encapsulation program

---
+++ Encapsulation/Lec24.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// virtual functions
+//class A{
+//	int x =5;
+//	public:
+//		virtual void display(){
+//			cout<<"Value of x is : "<< x<<endl;
+//		}
+//};
+//
+//class B : public A{
+//	int y =10;
+//	public:
+//		void display(){
+//			cout<<"Value of y is : "<< y<<endl;
+//		}
+//};
+//
+//int main(){
+//	A *a;
+//	B b;
+//	a = &b;
+//	a->display();
+//	b.display();
+//}
+
+
+
